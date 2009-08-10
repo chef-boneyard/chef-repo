97cb7815a6dd18158e248685d71696a35861d8f3
jtimberman <joshua@opscode.com>
2009-08-10 15:41:23 -0600

    chef-475, add chef_upgrade role

---
+++ roles/chef_upgrade.rb
@@ -0,0 +1,14 @@
+name "chef-upgrade"
+description "Use this role to upgrade Chef clients and server with the Opscode Chef cookbook. Requires that nodes are using chef::client or chef::server recipes."
+
+# Update the version number to upgrade Chef to a newer version.
+# You may need to add an override attribute under 'chef' for 'server_path'
+# depending on your installation, eg:
+#   "server_path" => "/usr/lib/ruby/gems/1.8/gems/chef-server-0.7.8"
+# On the line after server_version. Don't forget the comma ;).
+override_attributes(
+  "chef" => {
+    "client_version"=>"0.7.8",
+    "server_version"=>"0.7.8"
+  }
+)
