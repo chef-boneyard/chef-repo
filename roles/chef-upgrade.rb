aee1707187d2a9e8f61e2c1553c1ca9db5e0c0ae
jtimberman <joshua@opscode.com>
2009-09-23 13:49:17 -0600

    bump chef version

--- roles/chef-upgrade.rb
+++ roles/chef-upgrade.rb
@@ -4,11 +4,11 @@ description "Use this role to upgrade Chef clients and server with the Opscode C
 # Update the version number to upgrade Chef to a newer version.
 # You may need to add an override attribute under 'chef' for 'server_path'
 # depending on your installation, eg:
-#   "server_path" => "/usr/lib/ruby/gems/1.8/gems/chef-server-0.7.8"
+#   "server_path" => "/usr/lib/ruby/gems/1.8/gems/chef-server-0.7.10"
 # On the line after server_version. Don't forget the comma ;).
 override_attributes(
   "chef" => {
-    "client_version"=>"0.7.8",
-    "server_version"=>"0.7.8"
+    "client_version"=>"0.7.10",
+    "server_version"=>"0.7.10"
   }
 )
