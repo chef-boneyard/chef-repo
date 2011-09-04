478354badf0841ec4c1dcbaaeea251a809ce2ebc
Larry Wright <larrywright@gmail.com>
2011-09-04 13:31:28 -0500

    Made the elasticsearch config dynamic

--- cookbooks/elasticsearch/attributes/default.rb
+++ cookbooks/elasticsearch/attributes/default.rb
@@ -1,15 +1,32 @@
-set_unless[:elasticsearch][:version] = "0.17.1"
-set_unless[:elasticsearch][:checksum] = '67a5b6240c27db666b5d2b48cdc26b91d64e8f2e950c6383273e47a6f4020da4'
-set_unless[:elasticsearch][:datadir] = '/var/lib/elasticsearch/data'
+default[:elasticsearch][:version] = "0.17.1"
+default[:elasticsearch][:checksum] = '67a5b6240c27db666b5d2b48cdc26b91d64e8f2e950c6383273e47a6f4020da4'
+default[:elasticsearch][:datadir] = '/var/lib/elasticsearch/data'
 
-set_unless[:elasticsearch][:cluster_name] = "default"
+default[:elasticsearch][:cluster_name] = "default"
 
-set_unless[:elasticsearch][:home] = "/usr/lib/elasticsearch"
+default[:elasticsearch][:home] = "/usr/lib/elasticsearch"
 
-set_unless[:elasticsearch][:s3_gateway_bucket] = nil # s3 bucket to store gateway snapshots in
+default[:elasticsearch][:s3_gateway_bucket] = nil # s3 bucket to store gateway snapshots in
 
-set_unless[:elasticsearch][:heap_size] = 1000
-set_unless[:elasticsearch][:fd_ulimit] = nil # nofiles limit (make this something like 32768, see /etc/security/limits.conf)
+default[:elasticsearch][:heap_size] = 1000
+default[:elasticsearch][:fd_ulimit] = nil # nofiles limit (make this something like 32768, see /etc/security/limits.conf)
 
-set_unless[:elasticsearch][:default_replicas] = 1 # replicas are in addition to the original, so 1 replica means 2 copies of each shard
-set_unless[:elasticsearch][:default_shards] = 6 # 6*2 shards per index distributes evenly across 3, 4, or 6 nodes
+default[:elasticsearch][:default_replicas] = 1 # replicas are in addition to the original, so 1 replica means 2 copies of each shard
+default[:elasticsearch][:default_shards] = 6 # 6*2 shards per index distributes evenly across 3, 4, or 6 nodes
+
+default[:elasticsearch][:startup_timeout]   = 300
+default[:elasticsearch][:shutdown_timeout]  = 300
+default[:elasticsearch][:ping_timeout]      = 300
+default[:elasticsearch][:java_command]      = "java"
+
+default[:elasticsearch][:wrapper_name]      = "ElasticSearch"
+
+default[:elasticsearch][:path_work] = "/var/lib/elasticsearch/work"
+default[:elasticsearch][:path_logs] = "/var/log/elasticsearch"
+
+default[:elasticsearch][:http_enabled] = true
+default[:elasticsearch][:node_data] = true
+default[:elasticsearch][:recovery_after_nodes] = 1
+default[:elasticsearch][:recovery_after_time] = 5m
+default[:elasticsearch][:expected_nodes] = 2
+default[:elasticsearch][:gateway_fs_location] = "/var/lib/elasticsearch/gateway"
--- cookbooks/elasticsearch/recipes/default.rb
+++ cookbooks/elasticsearch/recipes/default.rb
@@ -7,9 +7,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -93,12 +93,13 @@ if node[:ec2]
   end
 end
 
-bash "copy elasticsearch conf" do
-  user "root"
-  cwd "/usr/lib/elasticsearch"
-  code %(cp -R ./config/* /etc/elasticsearch)
-  not_if { File.exists? "/etc/elasticsearch/logging.yml" }
+template "/etc/elasticsearch/elasticsearch.conf" do
+  source "elasticsearch.conf.erb"
+  mode 0644
 end
+
+end
+
 
 template "/etc/elasticsearch/logging.yml" do
   source "logging.yml.erb"
