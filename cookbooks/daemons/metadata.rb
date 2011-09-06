fb1dfd3cacdc3b2de86ea027faf3ff137472e199
Larry Wright <larrywright@gmail.com>
2011-09-05 22:23:07 -0500

    Added a new cookbook for daemonizing scripts

---
+++ cookbooks/daemons/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "CyByL"
+maintainer_email "larrywright@argonautlabs.com"
+license          "All rights reserved"
+description      "Installs/Configures daemons"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+depends          "runit"
