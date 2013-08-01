b479a4d34e3cc7970c7deca8093ab59923e26cd5
Robby Grossman <robby@freerobby.com>
2013-08-01 17:26:18 -0400

    Create ubuntu user if it does not already exist (ahem, Rackspace).

--- roles/wistia-base.rb
+++ roles/wistia-base.rb
@@ -1,22 +1,27 @@
 name 'wistia-base'
 description 'base configuration on top of which all wistia boxes are constructed'
+
+SSH_USERS = %w(
+  jim-bancroft
+  robby-grossman
+  max-kohl
+  jordan-munson
+  ben-ruedlinger
+  mary-schmidt
+  max-schnur
+  brendan-schwartz
+  jeff-vincent
+)
+
 run_list(
+  'recipe[wistia-base]',
   'recipe[ssh-keys]'
 )
 
 default_attributes(
   'ssh_keys' => {
     # Please keep users sorted by last name
-    'ubuntu' => %w(
-      jim-bancroft
-      robby-grossman
-      max-kohl
-      jordan-munson
-      ben-ruedlinger
-      mary-schmidt
-      max-schnur
-      brendan-schwartz
-      jeff-vincent
-    )
+    'ubuntu' => SSH_USERS,
+    'root' => SSH_USERS
   }
 )
