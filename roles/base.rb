c2aed2a4c8fdd277aae7b0376df6bace46754a7e
Arash Ghoreyshi <arashghoreyshi@gmail.com>
2013-08-15 11:04:06 -0500

    Make cloudpassage pick up the provided key

--- roles/base.rb
+++ roles/base.rb
@@ -1,18 +1,18 @@
 name 'base'
 description 'Base role for all servers'
 run_list(
-    'recipe[chef-cloudpassage]',
+    'recipe[cloudpassage]',
     'recipe[newrelic::repository]',
     'recipe[newrelic::server-monitor]',
     'recipe[yum::epel]'
 )
 override_attributes(
     'cloudpassage' => {
-        'license_key' => 'TODO: get from data bag or provide here'
+        'license_key' => 'get from a data bag or provide key here'
     },
     'newrelic' => {
         'server_monitoring' => {
-            'license' => 'TODO: get from data bag or provide here',
+            'license' => 'get from a data bag or provide key here',
             'logfile' => '/var/log/barbican/newrelic.log',
             'loglevel' => 'debug'
         }
