9e162d6e6cd52784f0a2c48007c3ce741971124d
Tao Ge <taoyge@gmail.com>
2013-09-12 17:26:34 -0700

    update version of libvpx

--- libvpx/attributes/default.rb
+++ libvpx/attributes/default.rb
@@ -25,4 +25,4 @@ default[:libvpx][:prefix] = "/usr/local"
 
 # JW 07-06-11: Hash of commit or a HEAD should be used - not a tag. Sync action of Git
 # provider will always attempt to update the git clone if a tag is used.
-default[:libvpx][:git_revision]   = "0491c2cfc89cded04de386ae691654c7653aac9b" # v0.9.6
+default[:libvpx][:git_revision]   = "b9ce43029298182668d4dcb0e0814189e4a63c2a" # v1.2.0
