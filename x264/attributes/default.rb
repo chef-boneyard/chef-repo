9153bcdf293e98aaf8e7a41379f9a691dbabf800
Tao Ge <taoyge@gmail.com>
2013-09-13 11:07:38 -0700

    try using package install of x264

--- x264/attributes/default.rb
+++ x264/attributes/default.rb
@@ -19,7 +19,7 @@
 # limitations under the License.
 #
 
-default[:x264][:install_method] = :source
+default[:x264][:install_method] = :package
 default[:x264][:git_repository] = "git://git.videolan.org/x264"
 default[:x264][:prefix] = "/usr/local"
 default[:x264][:compile_flags] = [
