44997ee695e67a5ebfed6421844e82673a1f8431
Tao Ge <taoyge@gmail.com>
2013-09-23 13:34:04 -0700

    lock ruby to 2.0.0

--- ruby/attributes/ruby.rb
+++ ruby/attributes/ruby.rb
@@ -1,32 +1,19 @@
 include_attribute 'opsworks_initial_setup::default'
 include_attribute 'opsworks_commons::default'
 
-# ruby_version 1.8.7 is handled by 'ruby-enterprise'
-case node["opsworks"]["ruby_version"]
-when "2.0.0"
-  default[:ruby][:major_version] = '2.0'
-  default[:ruby][:full_version] = '2.0.0'
-  default[:ruby][:patch] = 'p247'
-  default[:ruby][:pkgrelease] = '1'
-when "1.9.3"
-  default[:ruby][:major_version] = '1.9'
-  default[:ruby][:full_version] = '1.9.3'
-  default[:ruby][:patch] = 'p448'
-  default[:ruby][:pkgrelease] = '1'
-else
-  default[:ruby][:major_version] = '2.0'
-  default[:ruby][:full_version] = '2.0.0'
-  default[:ruby][:patch] = 'p247'
-  default[:ruby][:pkgrelease] = '1'
-end
 
-default[:ruby][:version] = "#{node[:ruby][:full_version]}#{node[:ruby][:patch]}"
+default[:ruby][:major_version] = '2.0'
+default[:ruby][:full_version] = '2.0.0'
+default[:ruby][:patch] = 'p247'
+default[:ruby][:pkgrelease] = '1'
+
+default[:ruby][:version] = "#{default[:ruby][:full_version]}#{default[:ruby][:patch]}"
 default[:ruby][:executable] = '/usr/local/bin/ruby'
 
 arch = RUBY_PLATFORM.match(/64/) ? 'amd64' : 'i386'
-default[:ruby][:deb] = "ruby#{node[:ruby][:major_version]}_#{node[:ruby][:full_version]}-#{node[:ruby][:patch]}.#{node[:ruby][:pkgrelease]}_#{arch}.deb"
-default[:ruby][:deb_url] = "#{node[:opsworks_commons][:assets_url]}/packages/#{node[:platform]}/#{node[:platform_version]}/#{node[:ruby][:deb]}"
+default[:ruby][:deb] = "ruby#{default[:ruby][:major_version]}_#{default[:ruby][:full_version]}-#{default[:ruby][:patch]}.#{default[:ruby][:pkgrelease]}_#{arch}.deb"
+default[:ruby][:deb_url] = "#{default[:opsworks_commons][:assets_url]}/packages/#{default[:platform]}/#{default[:platform_version]}/#{default[:ruby][:deb]}"
 
 rhel_arch = RUBY_PLATFORM.match(/64/) ? 'x86_64' : 'i686'
-default[:ruby][:rpm] = "ruby#{node[:ruby][:major_version].delete('.')}-#{node[:ruby][:full_version]}-#{node[:ruby][:patch]}-#{node[:ruby][:pkgrelease]}.#{rhel_arch}.rpm"
-default[:ruby][:rpm_url] = "#{node[:opsworks_commons][:assets_url]}/packages/#{node[:platform]}/#{node[:platform_version]}/#{node[:ruby][:rpm]}"
+default[:ruby][:rpm] = "ruby#{default[:ruby][:major_version].delete('.')}-#{default[:ruby][:full_version]}-#{default[:ruby][:patch]}-#{default[:ruby][:pkgrelease]}.#{rhel_arch}.rpm"
+default[:ruby][:rpm_url] = "#{default[:opsworks_commons][:assets_url]}/packages/#{default[:platform]}/#{default[:platform_version]}/#{default[:ruby][:rpm]}"
