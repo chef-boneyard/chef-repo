172a0786367efbf4344575b6573ac9323cf685a8
Douglas Mendizabal <douglas.mendizabal@rackspace.com>
2013-10-03 14:27:48 -0500

    Rename barbican-queue to barbican-rabbitmq

---
+++ cookbooks/barbican-rabbitmq/metadata.rb
@@ -0,0 +1,8 @@
+name             'barbican-rabbitmq'
+maintainer       'Rackspace, Inc.'
+maintainer_email 'cloudkeep@googlegroups.com'
+license          'Apache 2.0'
+description      'Installs/Configures barbican-rabbitmq'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
