f66055554247037d96e04055688993424ecd1446
Douglas Mendizabal <douglas.mendizabal@rackspace.com>
2013-10-03 14:11:39 -0500

    Rename barbican-db to barbican-postgresql.

--- cookbooks/barbican-db/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'barbican-db'
-maintainer       'Rackspace, Inc.'
-maintainer_email 'john.wood@rackspace.com'
-license          'Apache License, Version 2.0'
-description      'Installs/Configures barbican-db'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.2'
-depends          'barbican-base'
