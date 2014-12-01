dd4c0df17c400239deacd0b165b6337e68d8ac2e
mlev <levmichael3@gmail.com>
2014-12-01 14:26:12 +0200

    Import yum version 3.5.1

---
+++ cookbooks/yum/metadata.rb
@@ -0,0 +1,12 @@
+name 'yum'
+maintainer 'Chef'
+maintainer_email 'cookbooks@getchef.com'
+license 'Apache 2.0'
+description 'Configures various yum components on Red Hat-like systems'
+version '3.5.1'
+
+supports 'redhat'
+supports 'centos'
+supports 'scientific'
+supports 'amazon'
+supports 'fedora'
