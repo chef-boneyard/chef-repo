b8b9d0998a67bd6da95846038463d3fd49d1d233
mlev <levmichael3@gmail.com>
2014-12-02 15:48:20 +0200

    override nodes that on environment STAGING: use NTP servers in ASIA

--- environments/staging.rb
+++ environments/staging.rb
@@ -1,7 +1,9 @@
 name "STAGING"
 description 'The staging environment'
 
-override_attributes(
-  :ntp => { :servers => "0.europe.pool.ntp.org"}
-)
+override_attributes ({
+    "ntp" => {
+      "servers" =>  ["0.asia.pool.ntp.org", "1.asia.pool.ntp.org", "2.asia.pool.ntp.org", "3.asia.pool.ntp.org"] 
+    },
+})
 
