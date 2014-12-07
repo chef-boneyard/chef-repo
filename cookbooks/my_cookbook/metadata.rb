8905334b1bc13adc057fa003610ff5b9834f11f4
mlev <levmichael3@gmail.com>
2014-12-07 19:40:30 +0200

    Flowdock 1.1.7

--- cookbooks/my_cookbook/metadata.rb
+++ cookbooks/my_cookbook/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'levmichael3@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures my_cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.3'
 
 #added
 depends "chef-client"
