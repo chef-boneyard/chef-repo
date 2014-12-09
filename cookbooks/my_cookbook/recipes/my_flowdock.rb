26792652853fa67062c4b0c21561e6d9aca36d4c
mlev <levmichael3@gmail.com>
2014-12-09 20:28:04 +0200

    commit

--- cookbooks/my_cookbook/recipes/my_flowdock.rb
+++ cookbooks/my_cookbook/recipes/my_flowdock.rb
@@ -3,12 +3,19 @@ include_recipe 'chef_handler'
 
 gem_package('chef-handler-flowdock'){action :nothing}.run_action(:install)
 
-file '/var/chef/handlers/flowdock.rb' do
-#   source 'flowdock' 
-   mode 0755
- end
+template '/var/chef/handlers/flowdock.rb' do
+  source 'flowdock.rb.erb' 
+  mode 0755
+end
 
 
 
-#api_token => "536e1ad158d6bef37e0b1b77590da91a",
+
+#template '/etc/chef/client.rb' do
+#  source 'client.rb.erb' 
+#  variables(
+#    api_token: '536e1ad158d6bef37e0b1b77590da91a'
+#  )
+#end
+
 
