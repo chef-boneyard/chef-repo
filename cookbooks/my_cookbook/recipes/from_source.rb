cbeff94aa722529a327210432180821bab53da52
mlev <levmichael3@gmail.com>
2014-12-15 12:29:52 +0200

    Added remote_file to from_source recipe: Will only get the nginx tar.gz file if we don't have it allready

--- cookbooks/my_cookbook/recipes/from_source.rb
+++ cookbooks/my_cookbook/recipes/from_source.rb
@@ -1,9 +1,15 @@
 version = "1.4.6"
 
+#remote_file will fetch the file only if it's not here already
+remote_file "fetch_nginx_source" do
+  source "http://nginx.org/download/nginx-#{version}.tar.gz"
+  path "#{Chef:Config['file_cache_path']}/nginx-#{version}.tar.gz"
+end
+
+
 bash "Install NGINX from source" do
   cwd Chef::Config['file_cache_path']
   code <<-EOH
-    wget http://nginx.org/download/nginx-#{version}.tar.gz
     tar zxf nginx-#{version}.tar.gz &&
     cd nginx-#{version} &&
     ./configure --without-http_rewrite_module    --without-http_gzip_module && make && make install
