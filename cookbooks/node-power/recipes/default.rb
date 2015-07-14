39e9926b48008f98b1986f4d4b15f4108d8602bd
root <root@ip-172-31-47-222.us-west-2.compute.internal>
2015-07-14 01:19:00 +0000

    Updated and fixed bugs

--- cookbooks/node-power/recipes/default.rb
+++ cookbooks/node-power/recipes/default.rb
@@ -26,6 +26,9 @@ end
 
 patched_kernel_url = "https://s3-us-west-2.amazonaws.com/dmdu-cloudlab/kernel-3.13.11-ckt20.tar.gz"
 patched_kernel_tar = "/tmp/kernel-3.13.11-ckt20.tar.gz"
+i2c_module_url = "https://s3-us-west-2.amazonaws.com/dmdu-cloudlab/i2c.tar.gz"
+i2c_module_dir = "/lib/modules/3.13.11-ckt20"
+i2c_module_tar = "#{i2c_module_dir}/i2c.tar.gz"
 
 # download the tarball (or use exisiting one if the checksum matches)
 remote_file patched_kernel_tar do
@@ -43,6 +46,20 @@ end
 # extract the tarball into /boot
 execute "tar xf #{patched_kernel_tar}" do
   cwd '/boot'
+end
+
+directory i2c_module_dir do
+  action :create
+end
+
+# obtain tarball with i2c module
+remote_file i2c_module_tar do
+  source i2c_module_url
+end
+
+# extract the tarball 
+execute "tar xf #{i2c_module_tar}" do
+  cwd '/lib/modules/3.13.11-ckt20'
 end
 
 # Get information on the newest versions of packages
